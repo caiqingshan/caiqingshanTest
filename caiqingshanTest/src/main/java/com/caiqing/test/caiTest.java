5de2245a24c9fc5ec61fe006fd8324c96ccde5fb
caiqingshan <caiqingshan01>
2021-01-28 22:28:29 +0800

    add caiqingshanTest

---
+++ caiqingshanTest/src/main/java/com/caiqing/test/caiTest.java
@@ -0,0 +1,8 @@
+package com.caiqing.test;
+
+public class caiTest {
+    public static void main(String[] args) {
+        String a = "qqqqqqqqqqqqqqqqwwwww";
+        System.out.println(a);
+    }
+}
